6c8fd0df096a2d7a8cf592872f8de58a237e5436
victorschumann <schumann@tutamail.com>
2022-09-16 16:23:49 -0300

    Add intro & templates

---
+++ namevariation-answers.js
@@ -0,0 +1,35 @@
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    // number of characters in: name
+
+}
+
+/** @param {string} name */
+export function getFirstChar(name) {
+    // first character of: name
+
+}
+
+/** @param {string} name */
+export function getLastChar(name) {
+    // last character of: name
+
+}
+
+/** @param {string} name */
+export function getLower(name) {
+    // name all in lower case (example: "ABC" becomes "abc")
+
+}
+
+/** @param {string} name */
+export function getUpper(name) {
+    // name all in upper case (example: "abc" becomes "ABC")
+
+}
+
+/** @param {string} name */
+export function getCapitalized(name) {
+    // capitalized version of name (example: "alEX" becomes "Alex")
+
+}
